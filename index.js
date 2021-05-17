8d2cce06f76a96808c579e5f80f5eed768b8b4e3
dani6902 <60360349+dani6902@users.noreply.github.com>
2021-05-17 15:39:56 +0200

    forside

--- index.js
+++ index.js
@@ -1,10 +1,22 @@
 console.log("JS is running");
 
+const header = document.querySelector('.navbar')
+
+window.onscroll = function(){
+var top = window.scrollY;
+if (top >= 50){
+  header.classList.add('active')
+}else{
+  header.classList.remove('active');
+}
+}
+
+
 $(".carousel").owlCarousel({
   margin: 10,
   loop: true,
   autoplay: true,
-  autoplayTimeout: 2000,
+  autoplayTimeout: 4000,
   autoplayHoverPause: true,
   responsive: {
     0:{
